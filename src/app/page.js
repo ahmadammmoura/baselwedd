d40ee1e6aab603ce2bf3af8a3dacf5beb769101c
ahmad ammoura <ahmad@ahmads-MacBook-Pro-2.local>
2023-09-08 16:32:50 +0300

    full

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
         <title>M & B Landing Page</title>
         <meta name="description" content="M & B Landing Page" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
       </Head>
 
       <main>
