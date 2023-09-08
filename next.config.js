9a96da624ffb4c74264634280cb38909daa6a886
ahmad ammoura <ahmad@ahmads-MacBook-Pro-2.local>
2023-09-08 16:28:33 +0300

    da

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            // Add the viewport meta tag to prevent zooming
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'viewport',
-                value: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
-              },
-            ],
-          },
-        ];
-      },
+        async headers() {
+          return [
+            {
+              source: '/(.*)',
+              headers: [
+                {
+                  key: 'viewport',
+                  value: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+                },
+              ],
+            },
+          ];
+        },
 }
 
 module.exports = nextConfig
