9655fb51fbaad8a8bff57d2f26a8953c9f61ab97
ahmad ammoura <ahmad@ahmads-MacBook-Pro-2.local>
2023-09-08 16:14:25 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -12,14 +12,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <div style={{width:"100%", height:"500px",border:"1px solid red"}}>
+        <div style={{margin:"auto",position:"relative",width:"100%", height:"100vh",border:"1px solid red"}}>
           <LetterForm />
         </div>
-        <div style={{width:"100%", height:"400px",border:"1px solid red"}}>
+        {/* <div style={{width:"100%", height:"400px",border:"1px solid red"}}>
           <div style={{width:"100px",height:"100px",border:"1px solid red"}}>
 
           </div>
-        </div>
+        </div> */}
       </main>
 
       <footer>
