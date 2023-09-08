d3d6cad13905bc22cb587346842e07ba2544bc6c
ahmad ammoura <ahmad@ahmads-MacBook-Pro-2.local>
2023-09-08 16:18:33 +0300

    save

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            // Add the viewport meta tag to prevent zooming
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'viewport',
+                value: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+              },
+            ],
+          },
+        ];
+      },
+}
 
 module.exports = nextConfig
